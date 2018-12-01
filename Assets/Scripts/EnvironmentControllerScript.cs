0e6b1fc9e3716e56b430db3326a40cb8d4fee114
2door <tudor.cismarescu@gmail.com>
2018-12-01 14:04:05 +0000

    feat: adds camera control to follow player

---
+++ Assets/Scripts/EnvironmentControllerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentControllerScript : MonoBehaviour {
+
+	public GameObject startPlatformLocation;
+	public GameObject startPlatformPrefab;
+	public GameObject playerPrefab;
+	public GameObject playerSpawnLocation;
+	
+	// Use this for initialization
+	void Start () {
+		Instantiate(startPlatformPrefab, startPlatformLocation.transform.position, Quaternion.identity);
+		Instantiate(playerPrefab, playerSpawnLocation.transform.position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/ViewController.cs
+++ Assets/Scripts/ViewController.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class ViewController : MonoBehaviour {
 
+	public float playerLayer;
+
+	private GameObject playerObject;
+	private Camera mainCam;
+	
 	// Use this for initialization
 	void Start () {
 		Screen.SetResolution(700, 1000, false);
+		GameObject[] playerObjects = GameObject.FindGameObjectsWithTag("Player");
+		if(playerObjects == null) {
+			// Handle missing PO
+		} else if(playerObjects.Length > 1) {
+			// Delete all POs except first
+		} else {
+			playerObject = playerObjects[0];
+		}
+
+		mainCam = Camera.main;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		lerpCameraIfPlayerDescended();
+	}
+
+	private void lerpCameraIfPlayerDescended() {
+		float cameraY = mainCam.transform.position.y;
+		float playerObjectY = playerObject.transform.position.y;
+		if(playerObjectY <= cameraY) {
+			Vector3 cameraPos = new Vector3(
+				mainCam.transform.position.x,
+				cameraY,
+				mainCam.transform.position.z
+			);
+			Vector3 destination = new Vector3(
+				mainCam.transform.position.x,
+				playerObjectY,
+				mainCam.transform.position.z
+			);
+			mainCam.transform.position = Vector3.Lerp(cameraPos, destination, 1);
+		}
 	}
 }
