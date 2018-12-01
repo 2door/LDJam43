b95381529586b62c81969d5df9d3795d7ed3d051
2door <tudor.cismarescu@gmail.com>
2018-12-01 14:49:02 +0000

    feat: restricts horizontal movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	private Camera mainCam;
+	private float playerZ;
+	
 	void Start () {
+		mainCam = Camera.main;
+		playerZ = this.transform.position.z;
 	}
 	
 	void FixedUpdate () {
@@ -15,6 +20,13 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	private void MovePlayer() {
-		this.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+		float xLimitLeft = mainCam.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+		float xLimitRigh = mainCam.ViewportToWorldPoint(new Vector3(1, 1, 0)).x;
+		float xChange = Input.GetAxis("Horizontal");
+		float newX = Mathf.Clamp(this.transform.position.x + xChange, xLimitLeft, xLimitRigh);
+		float newY = this.transform.position.y;
+		float newZ = playerZ;
+
+		this.transform.position = new Vector3(newX, newY, newZ);
 	}
 }
